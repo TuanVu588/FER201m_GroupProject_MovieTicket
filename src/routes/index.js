33c3ee00102e8f27feeb8df2475da384fefff00a
lamthienthan <144668899+lamthienthan@users.noreply.github.com>
2023-11-01 22:47:49 +0700

    create signup page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import HomePage from '@/pages/user/HomePage';
 import LoginPage from '@/pages/user/Login';
+import SignUpPage from '@/pages/user/SignUp';
 const publicRoutes = [
   { path: '/', component: HomePage },
-  { path: '/login',component: LoginPage}
+  { path: '/login',component: LoginPage},
+  { path: '/sign-up', component: SignUpPage },
 ];
 export { publicRoutes};
