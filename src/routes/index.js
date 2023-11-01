440291b7ea06d314d107af94701aae688fc598dc
lamthienthan <144668899+lamthienthan@users.noreply.github.com>
2023-11-01 22:41:11 +0700

    create Loginpage

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import HomePage from '@/pages/user/HomePage';
+import LoginPage from '@/pages/user/Login';
 const publicRoutes = [
   { path: '/', component: HomePage },
+  { path: '/login',component: LoginPage}
 ];
 export { publicRoutes};
