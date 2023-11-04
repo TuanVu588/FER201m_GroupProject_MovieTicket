7c5f59e06ffa7dbaadd28d465e17fe2c2e647210
NgTuanHung051103 <hocfronendvoinabatiicheese@gmail.com>
2023-11-04 23:43:13 +0700

    update component Checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,21 +1,101 @@
-
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Banner from '@/components/Banner';
 
-function CheckoutPage() {
-  
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so add 1
+const day = String(currentDate.getDate()).padStart(2, '0');
+const formattedDate = `${year}-${month}-${day}`;
 
-  const rowHTML = 
-    <tr className='border-bottom' >
+function CheckoutPage() {
+  const navigator = useNavigate();
+
+  const [formData, setFormData] = useState({
+    user_id: '',
+    full_name: '',
+    phone: '',
+    email: '',
+    address: '',
+    date_checkout: formattedDate,
+    cart: [],
+  });
+
+  const handleCheckout = () => {
+    axios
+      .post('http://localhost:9999/bills', formData)
+      .then((response) => {
+        alert('Đặt vé thành công');
+        localStorage.removeItem('ticket');
+        navigator('/history');
+      })
+      .catch((error) => {
+        alert('Error submitting data', error);
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    const cartResponse = localStorage.getItem('ticket');
+    const userResponse = localStorage.getItem('user');
+    let finalData = {};
+    if (cartResponse) {
+      let responseDecode = JSON.parse(cartResponse);
+      responseDecode = responseDecode?.filter((item) => {
+        const ticketDateString = `${item?.date}T${item?.time}`;
+        const ticketDate = new Date(ticketDateString);
+        const currentDateTime = new Date();
+        return ticketDate >= currentDateTime;
+      });
+      finalData = { ...finalData, cart: responseDecode };
+    }
+    if (userResponse) {
+      let responseDecode = JSON.parse(userResponse);
+      finalData = {
+        ...finalData,
+        full_name: responseDecode?.firstName + ' ' + responseDecode?.lastName,
+        phone: responseDecode?.phone,
+        email: responseDecode?.email,
+        address: responseDecode?.townCity,
+        user_id: responseDecode?.id,
+        date_checkout: formattedDate,
+      };
+    }
+    setFormData(finalData);
+  }, []);
+
+  console.log(formData);
+
+  const totalPrice = formData?.cart?.reduce(
+    (total, order) => total + order.price,
+    0,
+  );
+
+  const rowHTML = formData?.cart?.map((item, index) => (
+    <tr className='border-bottom' key={index}>
       <td className='d-flex align-items-center'>
         <div
           className='fw-bold'
-          style={{  fontWeight: '500', color: 'red', marginRight: '5px'}}>
-          tittle
+          style={{
+            fontWeight: '500',
+            color: 'red',
+            marginRight: '5px',
+          }}
+        >
+          {item?.movie_title}
         </div>
       </td>
-      <td className='text-end fw-bold'>10000</td>
+      <td className='text-end fw-bold'>${item?.price}</td>
     </tr>
- 
+  ));
 
   return (
     <>
@@ -27,37 +107,32 @@ function CheckoutPage() {
             <div className='card-body'>
               <form>
                 <div className='mb-3'>
-                  <label className='small mb-1' htmlFor='fullName'>
-                    Full Name
-                  </label>
-                  <input className='form-control' id='fullName'  name='fullName' type='text' placeholder='Enter your full name'
-                    value={formData.full_name}  onChange={handleInputChange}/>
+                  <label className='small mb-1' htmlFor='fullName'>Full Name</label>
+                  <input className='form-control' id='fullName' name='fullName' type='text'placeholder='Enter your full name'
+                    value={formData.full_name} onChange={handleInputChange}/>
                 </div>
                 <div className='row gx-3 mb-3'>
                   <div className='col-md-6'>
-                    <label className='small mb-1' htmlFor='phone'>
-                      Phone number
-                    </label>
-                    <input className='form-control' id='phone'  name='phone' type='text' placeholder='Enter your full name'
-                    value={formData.phone}  onChange={handleInputChange}/>
+                    <label className='small mb-1' htmlFor='phone'>Phone number</label>
+                    <input className='form-control' id='phone' name='phone'type='tel'placeholder='Enter your phone number'
+                      value={formData.phone}onChange={handleInputChange}/>
                   </div>
                   <div className='col-md-6'>
-                    <label className='small mb-1' htmlFor='email'>
-                      Email address
-                    </label>
-                    <input className='form-control' id='email'  name='email' type='text' placeholder='Enter your full name'
-                    value={formData.email}  onChange={handleInputChange}/>
+                    <label className='small mb-1' htmlFor='email'>Email address</label>
+                    <input className='form-control' id='email' name='email' type='email' placeholder='Enter your email'
+                      value={formData.email} onChange={handleInputChange} />
                   </div>
                 </div>
                 <div className='mb-3'>
-                  <label className='small mb-1' htmlFor='address'>
-                    Address
-                  </label>
-                  <textarea className='form-control' id='address' name='address'  type='text'
-                    placeholder='Enter your address' value={formData.address}  onChange={handleInputChange}/>
+                  <label className='small mb-1' htmlFor='address'>Address</label>
+                  <textarea className='form-control' id='address'  name='address' type='text'  placeholder='Enter your address'
+                    value={formData.address} onChange={handleInputChange}
+                  />
                 </div>
 
-                <button className='btn btn-info' type='button' onClick={handleCheckout}>Place Order</button>
+                <button className='btn btn-info' type='button'onClick={handleCheckout}>
+                  Place Order
+                </button>
               </form>
             </div>
           </div>
@@ -81,7 +156,7 @@ function CheckoutPage() {
                     <td>
                       <div className='fw-bold'>TOTAL</div>
                     </td>
-                    <td className='text-end font-weight-bold'>2000</td>
+                    <td className='text-end font-weight-bold'>${totalPrice}</td>
                   </tr>
                 </tbody>
               </table>
